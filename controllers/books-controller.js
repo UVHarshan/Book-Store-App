ab38deb68229b52a026e1b30feb15c9efdfb91ff
Uvindu Gunasekara <harshanuv@gmail.com>
2022-08-18 16:48:04 +0530

    Created a route for updating a book

--- controllers/books-controller.js
+++ controllers/books-controller.js
@@ -26,7 +26,7 @@ const getById = async (req, res, next) => {
     if (!book) {
         return res.status(404).json({ message: " No book found!" });
     }
-    return res.status(200).json({ book});
+    return res.status(200).json({ book });
 }
 
 const addBook = async (req, res, next) => {
@@ -54,7 +54,32 @@ const addBook = async (req, res, next) => {
 
 }
 
+const updateBook = async (req, res, next) => {
+    const id = req.params.id;
+    const { name, author, description, price, available } = req.body;
+    let book;
+
+    try {
+        book = await Book.findByIdAndUpdate(id, {
+            name,
+            author,
+            description,
+            price,
+            available
+        });
+        book = await book.save();
+    } catch (error) {
+        console.log(error);
+    }
+
+    if (!book) {
+        return res.status(404).json({ message: " Unable to update the book with this id!" });
+    }
+    return res.status(200).json({ book });
+}
+
 
 exports.getAllBooks = getAllBooks;
 exports.addBook = addBook;
 exports.getById = getById;
+exports.updateBook = updateBook;
